93b1bd58e26698648c2ec982ff9a7bbb9ea7aef6
Callum Cleary <callumcleary1@hotmail.co.uk>
2022-08-24 16:47:14 +0100

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+
+require 'faker'
+puts "cleaning database.."
+Review.destroy_all
+10.times do
+  restaurant = Restaurant.create(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.street_name,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+  # rating: rand(0..5)
+)
+  puts "restaurant with name: #{restaurant.name} has been created."
+end
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
