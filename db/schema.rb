1e34ee2679a84366be8a34458d4e7d552af36e97
Callum Cleary <callumcleary1@hotmail.co.uk>
2022-08-24 16:00:37 +0100

    Rails Yelp MVP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_112142) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_131740) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_112142) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
